ecaf3cda508b87ac5862d19bfade200117d2602f
Jacob <jacobamelse@hotmail.com>
2023-01-01 17:58:51 -0600

    commit tree 4adb6ad9691af127f0fde96bb176017a33730377

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
+class Book
+  attr_accessor :page
+  attr_accessor :author
+  attr_accessor :page_count
+  attr_accessor :genre
+  attr_reader :turn_page
+  def initialize(title)
+    @title = title
+  end
+  def title
+    @title
+  end
+  def turn_page
+   puts "Flipping the page...wow, you read fast!"
+  end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+  def brand 
+    @brand
+  end
+  def condition=(condition)
+    @conditon = condition
+  end
+  def condition
+    @conditon
+  end
+  def cobble
+    @conditon = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
